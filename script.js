560eb1dceecc4da6c2c554c6d2b25e911335cb87
brandstudy2026 <francescaavallone54@gmail.com>
2025-07-07 15:31:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const surveyUrl = 'https://example.com/survey';
+  const surveyUrl = 'https://unc.az1.qualtrics.com/jfe/form/SV_dpshwYR13RpWonQ';
   document.getElementById('return-btn').addEventListener('click', () => {
     window.location.href = surveyUrl;
   });
